f78c699843297894eaa59d56e2902010fc2cb079
DeaR <nayuri@kuonn.mydns.jp>
2015-07-16 01:30:28 +0900

    Vim: Refactoring.

--- .vim/tools/vimproc/build.bat
+++ .vim/tools/vimproc/build.bat
@@ -1,20 +1,18 @@
 @echo off
 
-if "%VCVARSALL%"=="" (
-  if not "%VS120COMNTOOLS%"=="" (
-    set "VCVARSALL=%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
-  ) else if not "%VS110COMNTOOLS%"=="" (
-    set "VCVARSALL=%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
-  ) else if not "%VS100COMNTOOLS%"=="" (
-    set "VCVARSALL=%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
-  ) else if not "%VS90COMNTOOLS%"=="" (
-    set "VCVARSALL=%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
-  ) else if not "%VS80COMNTOOLS%"=="" (
-    set "VCVARSALL=%VS80COMNTOOLS%..\..\VC\vcvarsall.bat"
-  ) else (
-    echo MSVC not found.
-    goto :EOF
-  )
+if not "%VS120COMNTOOLS%"=="" (
+  set "VCVARSALL=%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+) else if not "%VS110COMNTOOLS%"=="" (
+  set "VCVARSALL=%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
+) else if not "%VS100COMNTOOLS%"=="" (
+  set "VCVARSALL=%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
+) else if not "%VS90COMNTOOLS%"=="" (
+  set "VCVARSALL=%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
+) else if not "%VS80COMNTOOLS%"=="" (
+  set "VCVARSALL=%VS80COMNTOOLS%..\..\VC\vcvarsall.bat"
+) else (
+  echo MSVC not found.
+  goto :EOF
 )
 call "%VCVARSALL%" %PROCESSOR_ARCHITECTURE%
 
